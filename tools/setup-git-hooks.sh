e91dec626a25d38c983fc1fdd05293bc5f27c4c6
ptrvsrg <s.petrov1@g.nsu.ru>
2024-09-23 05:28:20 +0700

    [develop] Add scripts for preparing Git hooks and building app and Docker image

---
+++ tools/setup-git-hooks.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+                                                                  
+cp hooks/prepare-commit-msg.sh .git/hooks/prepare-commit-msg
+
+chmod 755 .git/hooks/prepare-commit-msg
