4678d7f3f5f9b016e04d27ae6762c3918186060c
ptrvsrg <s.petrov1@g.nsu.ru>
2024-09-25 22:25:36 +0700

    [develop] Refactoring; Extract HackathonWorker class

---
+++ DreamTeamOptimizer.ConsoleApp/HackathonWorker.cs
@@ -0,0 +1,40 @@
+using DreamTeamOptimizer.Core.Entities;
+using DreamTeamOptimizer.Core.Helpers;
+using DreamTeamOptimizer.Strategies;
+using log4net;
+
+namespace DreamTeamOptimizer.ConsoleApp;
+
+public class HackathonWorker
+{
+    private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+    
+    public static void Run(string juniorsFilePath, string teamLeadsFilePath,
+        StrategyType strategyType = StrategyType.GaleShapley, int hackathonsCount = 1000)
+    {
+        var juniors = CsvLoader.Load<Employee>(juniorsFilePath);
+        var teamLeads = CsvLoader.Load<Employee>(teamLeadsFilePath);
+        var strategy = StrategyFactory.NewStrategy(strategyType);
+        var hrManager = new HRManager(strategy);
+        var hrDirector = new HRDirector();
+
+        var hackathonNum = 1;
+        var totalHarmonicity = 0.0;
+        var lockObj = new object();
+
+        Parallel.For(0, hackathonsCount, new ParallelOptions(), _ =>
+        {
+            var hackathon = new Hackathon(juniors, teamLeads, hrManager, hrDirector);
+            var harmonicity = hackathon.Start();
+
+            lock (lockObj)
+            {
+                totalHarmonicity += harmonicity;
+                var averageHarmonicity = totalHarmonicity / hackathonNum;
+                Logger.Info(
+                    $"Hackathon {hackathonNum}: harmonicity={harmonicity}, average_harmonicity={averageHarmonicity}");
+                hackathonNum++;
+            }
+        });
+    }
+}
--- DreamTeamOptimizer.ConsoleApp/Program.cs
+++ DreamTeamOptimizer.ConsoleApp/Program.cs
@@ -1,26 +1,25 @@
 ﻿using CommandLine;
-using DreamTeamOptimizer.Core;
+using DreamTeamOptimizer.Core.Entities;
+using DreamTeamOptimizer.Core.Helpers;
 using DreamTeamOptimizer.Strategies;
 using log4net;
 using log4net.Config;
 
 namespace DreamTeamOptimizer.ConsoleApp;
 
-class Program
+public class Program
 {
-    private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
-    
     private class Options
     {
         [Option('j', "juniors", Required = true, HelpText = "Path to the juniors CSV file.")]
-        public string JuniorsFilePath { get; set; }
+        public string JuniorsFilePath { get; set; } = null!;
 
         [Option('t', "teamleads", Required = true, HelpText = "Path to the team leads CSV file.")]
-        public string TeamLeadsFilePath { get; set; }
+        public string TeamLeadsFilePath { get; set; } = null!;
 
         [Option('s', "strategy", Default = StrategyType.GaleShapley,
             HelpText = "Strategy to use for team building (GaleShapley, BipartiteGraphWithRating).")]
-        public StrategyType Strategy { get; set; }
+        public StrategyType StrategyType { get; set; }
 
         [Option('n', "hackathons", Default = 1000, HelpText = "Number of hackathons to conduct.")]
         public int HackathonsCount { get; set; }
@@ -30,35 +29,7 @@ class Program
     {
         BasicConfigurator.Configure();
         Parser.Default.ParseArguments<Options>(args)
-            .WithParsed(Run);
-    }
-
-    private static void Run(Options options)
-    {
-        var juniors = CsvLoader.Load<Employee>(options.JuniorsFilePath);
-        var teamLeads = CsvLoader.Load<Employee>(options.TeamLeadsFilePath);
-        var strategy = StrategyFactory.NewStrategy(options.Strategy);
-        var hrManager = new HRManager(strategy);
-        var hrDirector = new HRDirector();
-
-        int hackathonNum = 1;
-        double totalHarmonicity = 0;
-        object lockObj = new object();
-
-        Parallel.For(0, options.HackathonsCount, new ParallelOptions(), _ =>
-        {
-            Hackathon hackathon = new Hackathon(juniors, teamLeads, hrManager, hrDirector);
-            double harmonicity = hackathon.Start();
-
-            lock (lockObj)
-            {
-                totalHarmonicity += harmonicity;
-                Logger.Info($"Hackathon {hackathonNum} harmonicity: {harmonicity}");
-                hackathonNum++;
-            }
-        });
-
-        double averageHarmonicity = totalHarmonicity / options.HackathonsCount;
-        Logger.Info($"Average harmonicity across all hackathons: {averageHarmonicity}");
+            .WithParsed(options => HackathonWorker.Run(options.JuniorsFilePath, options.TeamLeadsFilePath,
+                options.StrategyType, options.HackathonsCount));
     }
 }
